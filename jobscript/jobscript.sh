84c47b3bd26f5b8daa5d87d87e37a41faa219126
aca10370eo <kaseikappatokunn2@yahoo.co.jp>
2019-09-05 10:17:37 +0900

    add jobscript

---
+++ jobscript/jobscript.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+#$-l rt_G.small=1
+#$ -l h_rt=10:00:00
+#$-cwd
+#$-j y
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4 cudnn/7.4/7.4.2
+
+cd ~/Documents/tensorflow20/
+
+python3 autoencoder.py
